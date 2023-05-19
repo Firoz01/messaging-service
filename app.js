220d93c194165bb0c9153c232e993f4e4c0f5751
Md Firoz Mia <67627130+Firoz01@users.noreply.github.com>
2023-05-19 21:55:32 +0600

    add Immediatley invoked function expresion and avoid golabl variable

--- app.js
+++ app.js
@@ -34,41 +34,46 @@ app.get('/', (req, res) => {
 app.use('/api/v1/chat', ChatRoute);
 app.use('/api/v1/message', MessageRoute);
 
-let activeUsers = [];
+const activeUsers = (function() {
+  let users = [];
+
+  return {
+    addUser: function(userId, socketId) {
+      if (!users.some((user) => user.userId === userId)) {
+        users.push({ userId, socketId });
+      }
+    },
+    removeUser: function(socketId) {
+      users = users.filter((user) => user.socketId !== socketId);
+    },
+    getUsers: function() {
+      return users;
+    }
+  };
+})();
 
 io.on('connection', (socket) => {
   console.log('client connected');
-  // add new User
+
   socket.on('new-user-add', (newUserId) => {
-    // if user is not added previously
-    if (!activeUsers.some((user) => user.userId === newUserId)) {
-      activeUsers.push({ userId: newUserId, socketId: socket.id });
-      //console.log("New User Connected", activeUsers);
-    }
-    // send all active users to new user
-    io.emit('get-users', activeUsers);
+    activeUsers.addUser(newUserId, socket.id);
+    io.emit('get-users', activeUsers.getUsers());
   });
 
-  // send message to a specific user
   socket.on('send-message', (data) => {
     const { receiverId } = data;
-    const user = activeUsers.find((user) => user.userId === receiverId);
-    //console.log("Sending from socket to :", receiverId);
-    //console.log("Data: ", data);
+    const user = activeUsers.getUsers().find((user) => user.userId === receiverId);
     if (user) {
       io.to(user.socketId).emit('receive-message', data);
-      //console.log("user found", user);
     }
   });
 
   socket.on('disconnect', () => {
-    // remove user from active users
-    activeUsers = activeUsers.filter((user) => user.socketId !== socket.id);
-    //console.log("User Disconnected", activeUsers);
-    // send all active users to all users
-    io.emit('get-users', activeUsers);
+    activeUsers.removeUser(socket.id);
+    io.emit('get-users', activeUsers.getUsers());
   });
 });
+
 
 app.all('*', (req, res, next) => {
   next(new AppError(`Can't find ${req.originalUrl} on this server`, 404));
